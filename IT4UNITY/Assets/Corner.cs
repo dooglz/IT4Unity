995ab1771e051622b7659fa920d7e95fcc992c26
Serrels <40082367@live.napier.ac.uk>
2016-05-30 17:12:55 +0100

    Corner code

---
+++ IT4UNITY/Assets/Corner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corner : Track {
+
+    // Use this for initialization
+    override protected void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	override protected void Update () {
+        base.Update();
+	}
+
+    public override Vector3 CalculateLaneTarget(Vector3 playerPos)
+    {
+        return (transform.position + (transform.right*(transform.localScale.x * 5.0f)*1.5f));
+    }
+
+    public override Vector3 CalculateNudge(Vector3 playerPos)
+    {
+        Vector3 lt = CalculateLaneTarget(playerPos);
+        //nudge.Scale(new Vector3(Mathf.Abs(trackGo.transform.right.x), Mathf.Abs(trackGo.transform.right.y), Mathf.Abs(trackGo.transform.right.z)));
+        Vector3 nudge = (lt - playerPos) * 0.05f;
+        nudge.y = 0.0f;
+        return nudge;
+    }
+
+    public override Vector3 GetAccelerateDirection(Vector3 playerPos)
+    {
+        //  return transform.right;
+        return Vector3.zero;
+    }
+
+}
--- IT4UNITY/Assets/Track.cs
+++ IT4UNITY/Assets/Track.cs
@@ -1,29 +1,65 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Track : MonoBehaviour {
-  public float speedboost = 1.0f;
-  public float landespread = 1.0f;
-  public Vector3 LaneOrigin;
-	// Use this for initialization
-	void Start () {
-    LaneOrigin = transform.position + transform.right * landespread;
-  }
+public
+class Track : MonoBehaviour
+{
+    public
+      float speedboost = 1.0f;
 
-  void OnValidate()
-  {
-    LaneOrigin = transform.position + transform.right * landespread;
-  }
+    public
+      float landespread = 1.0f;
 
-  void OnDrawGizmosSelected()
-  {
-    Gizmos.color = Color.red;
-    Gizmos.DrawWireSphere(LaneOrigin, 0.2f);
-    Gizmos.color = Color.white;
-  }
+    public
+      Vector3 LaneOrigin;
 
-  // Update is called once per frame
-  void Update () {
-    LaneOrigin = transform.position + transform.right * landespread;
-  }
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        LaneOrigin = transform.position + transform.right * landespread;
+    }
+
+    protected virtual void OnValidate()
+    {
+        LaneOrigin = transform.position + transform.right * landespread;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(LaneOrigin, 0.2f);
+        Gizmos.color = Color.blue; ;
+        Gizmos.DrawWireSphere(CalculateLaneTarget(transform.position), 0.2f);
+        Gizmos.color = Color.white;
+    }
+
+    public virtual Vector3 GetAccelerateDirection(Vector3 playerPos)
+    {
+        return transform.forward;
+    }
+
+    public virtual Vector3 CalculateNudge(Vector3 playerPos)
+    {
+        Vector3 lt = CalculateLaneTarget(playerPos);
+        //nudge.Scale(new Vector3(Mathf.Abs(trackGo.transform.right.x), Mathf.Abs(trackGo.transform.right.y), Mathf.Abs(trackGo.transform.right.z)));
+        Vector3 nudge = (lt - playerPos) * 0.1f;
+        //never nudge Y
+        nudge.y = 0.0f;
+        return nudge;
+    }
+
+    public virtual Vector3 CalculateLaneTarget(Vector3 playerPos)
+    {
+        Vector2 of = new Vector2(transform.forward.x, transform.forward.z);
+        Vector2 oc = new Vector2(playerPos.x - transform.position.x,
+                                 playerPos.z - transform.position.z);
+        float ang = Vector2.Angle(of, oc);
+        float dist = Mathf.Cos(ang * Mathf.Deg2Rad) * oc.magnitude;
+        return (LaneOrigin + (transform.forward * dist));
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        LaneOrigin = transform.position + transform.right * landespread;
+    }
 }
