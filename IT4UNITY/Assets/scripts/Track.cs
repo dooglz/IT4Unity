15a172132a779d460802afe13fe228750e416fad
Serrels <40008190@napier.ac.uk>
2016-06-07 20:02:41 +0100

    lane switching working

--- IT4UNITY/Assets/scripts/Track.cs
+++ IT4UNITY/Assets/scripts/Track.cs
@@ -11,20 +11,33 @@ class Track : MonoBehaviour
 
     public
       Vector3 LaneOrigin;
+    public Material[] roadtextures;
 
+    private float lanewidth;
   public int lanes = 1;
     // Use this for initialization
     protected virtual void Start()
     {
-        float xx = ((4.0f / lanes) * 0.5f) - 2.0f;
-        LaneOrigin = transform.position + (transform.right * xx);
+        float roadsize = 5.0f;
+        lanewidth = roadsize / (lanes + 1.0f);
+        float totallanewidth = lanewidth * lanes * 0.5f;
+        LaneOrigin = transform.position - (transform.right * (totallanewidth)) + (transform.right * (lanewidth * 0.5f));
+        GetComponent<Renderer>().sharedMaterial = getMat();
+    }
+
+    protected virtual Material getMat()
+    {
+        return roadtextures[lanes-1];
     }
 
     protected virtual void OnValidate()
     {
-    float xx = ((4.0f / lanes) * 0.5f) - 2.0f;
-    LaneOrigin = transform.position + (transform.right * xx);
-  }
+        float roadsize = 5.0f;
+        lanewidth = roadsize / (lanes + 1.0f);
+        float totallanewidth = lanewidth * lanes * 0.5f;
+        LaneOrigin = transform.position - (transform.right * (totallanewidth)) + (transform.right * (lanewidth * 0.5f));
+        GetComponent<Renderer>().sharedMaterial = getMat();
+    }
 
     private void OnDrawGizmosSelected()
     {
@@ -41,13 +54,13 @@ class Track : MonoBehaviour
 
     public virtual Vector3 GetAccelerateDirection(Vector3 playerPos, int lane)
     {
-      if (lane > lanes) lane = lanes;
-    return transform.forward;
+        if (lane > lanes - 1) lane = lanes - 1;
+        return transform.forward;
     }
 
     public virtual Vector3 CalculateNudge(Vector3 playerPos, int lane)
     {
-     if (lane > lanes) lane = lanes;
+     if (lane > lanes-1) lane = lanes-1;
       Vector3 lt = CalculateLaneTarget(playerPos,lane);
         //nudge.Scale(new Vector3(Mathf.Abs(trackGo.transform.right.x), Mathf.Abs(trackGo.transform.right.y), Mathf.Abs(trackGo.transform.right.z)));
         Vector3 nudge = (lt - playerPos) * 0.1f;
@@ -58,7 +71,7 @@ class Track : MonoBehaviour
 
     public virtual Vector3 CalculateLaneTarget(Vector3 playerPos, int lane)
     {
-    if (lane > lanes) lane = lanes;
+        if (lane > lanes - 1) lane = lanes - 1;
         Vector2 of = new Vector2(transform.forward.x, transform.forward.z);
         Vector2 oc = new Vector2(playerPos.x - transform.position.x,
                                  playerPos.z - transform.position.z);
@@ -67,12 +80,12 @@ class Track : MonoBehaviour
         float min = transform.localScale.z * -4.8f;
         float max = transform.localScale.z * 5.1f;
         dist = Mathf.Min(Mathf.Max(dist, min),max);
-        return (LaneOrigin + (transform.forward * dist) + (transform.right * lane));
+        return (LaneOrigin + (transform.forward * dist) + (transform.right * lane * lanewidth));
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
-        LaneOrigin = transform.position + transform.right * landespread;
+      //  LaneOrigin = transform.position + transform.right * landespread;
     }
 }
